8400466d209ebfebab28ae05e54feba1ec23846a
Francesca <francesca@thriveweb.com.au>
2018-08-06 10:34:09 +1000

    Initial commit

---
+++ content/pages/home.md
@@ -0,0 +1,26 @@
+---
+template: HomePage
+slug: ''
+title: Hello World!
+featuredImage: /images/uploads/paint1.jpg
+subtitle: This is the home page subtitle test
+meta:
+  title: ''
+---
+
+# Whitesmoke
+
+An opinionated starter project for creating lightning-fast websites with [Gatsby](https://gatsbyjs.org) and [Netlify CMS](https://netlifycms.org).
+
+- **[Gatsby](https://gatsbyjs.org)** static site generator
+- **[Netlify CMS](https://github.com/netlify/netlify-cms)** for content management
+
+## See also
+
+[Netlify CMS Docs](https://www.netlifycms.org/docs/)  
+[Netlify CMS Repo](https://github.com/netlify/netlify-cms)  
+[Gatsby + Netlify CMS Starter](https://github.com/AustinGreen/gatsby-starter-netlify-cms)
+
+## Get going
+
+[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/thriveweb/whitesmoke&stack=cms)
---
+++ src/layouts/index.js
@@ -0,0 +1,52 @@
+import React, { Fragment } from 'react'
+import Helmet from 'react-helmet'
+
+import 'modern-normalize/modern-normalize.css'
+
+import './globalStyles.css'
+import Meta from '../components/Meta'
+import Nav from '../components/Nav'
+import Footer from '../components/Footer'
+import GithubCorner from '../components/GithubCorner'
+
+export default ({ children, data }) => {
+  const { siteTitle, siteUrl, socialMediaCard, headerScripts } =
+    data.settingsYaml || {}
+  return (
+    <Fragment>
+      <Helmet defaultTitle={siteTitle} titleTemplate={`%s | ${siteTitle}`}>
+        {/* Add font link tags here */}
+      </Helmet>
+
+      <Meta
+        headerScripts={headerScripts}
+        absoluteImageUrl={
+          socialMediaCard &&
+          socialMediaCard.image &&
+          siteUrl + socialMediaCard.image
+        }
+      />
+
+      <GithubCorner url="https://github.com/thriveweb/whitesmoke" />
+
+      <Nav />
+
+      <Fragment>{children()}</Fragment>
+
+      <Footer />
+    </Fragment>
+  )
+}
+
+export const query = graphql`
+  query IndexLayoutQuery {
+    settingsYaml {
+      siteTitle
+      siteDescription
+      headerScripts
+      socialMediaCard {
+        image
+      }
+    }
+  }
+`
