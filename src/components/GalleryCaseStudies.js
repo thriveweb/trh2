a1fe761871a01c0eada034010c5e3fb513de4334
Francesca <francesca@thriveweb.com.au>
2018-08-07 15:03:49 +1000

    update slider

--- src/components/GalleryCaseStudies.js
+++ src/components/GalleryCaseStudies.js
@@ -14,10 +14,6 @@ export default ({ galleryCaseStudies = [], alt = '' }) => {
     pagination: {
       el: '.swiper-pagination',
       clickable: true
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
     }
   }
 
