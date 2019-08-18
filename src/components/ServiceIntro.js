74bce128a98dd1196e5f44ce1752434cbe5b3c82
Francesca <francesca@thriveweb.com.au>
2019-08-19 08:59:11 +1000

    add content to services service intro

--- src/components/ServiceIntro.js
+++ src/components/ServiceIntro.js
@@ -2,25 +2,22 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import Image from '../components/Image'
+import Content from '../components/Content'
 
 import './ServiceIntro.scss'
 
-export default (props) => (
-    <div className="relative service-intro">
-        <div className="relative container">
-            <div className="service-single">
-                <h2>{props.title}</h2>
+export default props => (
+  <div className="relative service-intro">
+    <div className="relative container">
+      <div className="service-single">
+        <h2>{props.title}</h2>
 
-                <Image
-                    className="icon"
-                    src={props.icon}
-                    alt="vaccination icon"
-                />
+        <Image className="icon" src={props.icon} alt="vaccination icon" />
 
-                <p className="subtitle">
-                    {props.subtitle}
-                </p>
-            </div>
-        </div>
+        <p className="subtitle">
+          <Content source={props.subtitle} />
+        </p>
+      </div>
     </div>
+  </div>
 )
